3a548d0402907212f9b4639b222efa91fe80bf10
maria <emevala@gmail.com>
2018-01-22 10:39:28 +0100

    variable edad, string, boolean nuevas variables

--- app/src/main/java/com/example/didact/myapplication/MainActivity.java
+++ app/src/main/java/com/example/didact/myapplication/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int edad =3;
+        int edad =2;
         String nombre= "María";
         boolean flag = false;
     }
